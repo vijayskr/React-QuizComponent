cbecf8d6e38eab85a9b958cdb4a80eec4c128794
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2018-06-19 15:23:27 -0500

    QuizButton

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,24 +1,23 @@
 import React, {Component} from 'react'
+import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component
 {
     render() {
         return (
-
             <main>
                 <section>
                     <p>
                         { this.props.quiz_question.instruction_text } 
                     </p>
                 </section>
-        <section className="buttons">
-          <ul>
-            <li> 
-                {this.props.quiz_question.answer_options[0]}
-            </li>
-          </ul>
-        </section>
-      </main>
+                <section className="buttons">
+                    <ul>
+                    <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+
+                    </ul>
+                </section>
+            </main>
         )
     }
 }
---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class QuizQuestionButton extends Component
+{
+    render () {
+        return(
+        <li>
+            <button> 
+
+            </button>
+        </li>
+        )
+    }
+
+}
+
+export default QuizQuestionButton
