2e4852a4957300bec2f2010418c6cd6456ca1f61
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2018-06-19 20:29:15 -0500

    QuizEnd Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
@@ -9,10 +10,21 @@ class Quiz extends Component{
         super(props)
         this.state = {quiz_position: 1}
     }
+    
     render() {
+        const isQuizEnd = 'false' ? ((this.state.quiz_position - 1) === quizData.quiz_questions.length) : 'true';
+        let quizDisp;
+
+        if (isQuizEnd){
+            quizDisp = <QuizEnd />;
+        }
+        else {
+            quizDisp = <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />;
+        }
+
         return(
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+                {quizDisp}
             </div>
         )
     }
