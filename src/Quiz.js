88ac690b6c15b8915fee75479d3bfe25d731a6ba
VIJAYASANKAR BALASUBRAMANIAN <vijayasankar@outlook.com>
2018-06-20 21:37:24 -0500

    QuizEnd Update

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,12 +17,18 @@ class Quiz extends Component{
             return {quiz_position: state.quiz_position + 1}
         })
     }
+
+    handleResetClick()
+    {
+        this.setState({quiz_position: 1})
+        this.props.resetClickHandler()
+    }
     render() {
         const isQuizEnd = 'false' ? ((this.state.quiz_position - 1) === quizData.quiz_questions.length) : 'true';
         let quizDisp;
 
         if (isQuizEnd){
-            quizDisp = <QuizEnd />;
+            quizDisp = <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>;
         }
         else {
             quizDisp = <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>;
